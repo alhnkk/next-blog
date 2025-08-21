da03f410e2df58194d7753a42a689d159cb6e822
alhnkk <alhnkk@gmail.com>
2025-08-21 16:49:49 +0300

    New background image and buttons were added for the admin panel. Links were created for adding new posts, viewing messages, and category management. Additionally, the AppAreaChart component was replaced with AppBarChart.

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,19 +1,66 @@
-import { AppAreaChart } from "@/components/admin/app-area-chart";
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { CirclePlus, Folder, MessageCircle } from "lucide-react";
+import { useTheme } from "next-themes";
+
 import { AppBarChart } from "@/components/admin/app-bar-chart";
 import AppPieChart from "@/components/admin/app-pie-chart";
 import CardList from "@/components/admin/card-list";
 import TodoList from "@/components/admin/todo-list";
+import { Button } from "@/components/ui/button";
 
 const AdminDashboard = () => {
+  const { theme } = useTheme();
+  
+  const backgroundImage = theme === "light" 
+    ? "https://images.pexels.com/photos/313563/sand-pattern-wave-texture-313563.jpeg"
+    : "https://images.pexels.com/photos/2387793/pexels-photo-2387793.jpeg";
+
   return <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
-    <div className="bg-primary-foreground p-4 rounded-md lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-      <AppBarChart />
+    <div className="relative grid rounded-md lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+      <Image 
+      className="absolute grayscale-75 rounded-md z-0"
+      src={backgroundImage}
+      alt="Dashboard Background" 
+      fill />
+      {/* <AppAreaChart /> */}
+      <Image 
+      className="rounded-full z-0 flex m-auto"
+      src="/logo.jpeg" 
+      alt="Dashboard Background" 
+      width={256}
+      height={256}
+      />
+      <div className="grid grid-cols-10 z-50 gap-4 items-end m-4">
+        <Link href="/admin/posts/new" className="col-span-4">
+          <Button className="w-full bg-background/80 p-4 rounded-md h-24 text-foreground hover:text-background">
+            <CirclePlus />
+            Yeni Gönderi Ekle
+          </Button>
+        </Link>
+        <Link href="/admin/messages" className="col-span-3">
+          <Button className="w-full bg-background/80 p-4 rounded-md h-24 text-foreground hover:text-background">
+            <MessageCircle />
+            Mesajları Görüntüle
+          </Button>
+        </Link>
+        <Link href="/admin/categories" className="col-span-3">
+          <Button className="w-full bg-background/80 p-4 rounded-md h-24 text-foreground hover:text-background">
+            <Folder />
+            Kategori Yönetimi
+          </Button>
+        </Link>
+        
+      </div>
+
     </div>
     <div className="bg-primary-foreground p-4 rounded-md"><CardList title="Son Üyeler" /></div>
     <div className="bg-primary-foreground p-4 rounded-md"><AppPieChart /></div>
     <div className="bg-primary-foreground p-4 rounded-md"><TodoList /></div>
     <div className="bg-primary-foreground p-4 rounded-md lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-      <AppAreaChart />
+      <AppBarChart />
     </div>
     <div className="bg-primary-foreground p-4 rounded-md"><CardList title="Son Mesajlar" /></div>
   </div>
